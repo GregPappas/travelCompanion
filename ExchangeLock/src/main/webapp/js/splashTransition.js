4e6757267dffdeab5ba0c7257fc755c2d6d61427
Gregory Pappas <gregorypappas@Gregorys-MacBook-Pro.local>
2014-07-11 10:02:28 +0100

    added maps tag

--- ExchangeLock/src/main/webapp/js/splashTransition.js
+++ ExchangeLock/src/main/webapp/js/splashTransition.js
@@ -6,6 +6,11 @@ $.get( "home_page.html", function( data ) {
   $("#content_body").html( data );
 });
 
+
+$.get("map.html", function(data){
+    $("#map").html(data);
+})
+
 $.get("safetynet.html", function(data)
 {
     $("#safetynet").html(data);
@@ -14,17 +19,23 @@ $.get("currency_lock.html", function(data){
     $("#currency_lock").html(data);
 })
 
+
+
 $.get("footer.html", function(data)
 {
     $("#footer").html(data);
 })
+
+
 
 }
 
 
 
 function showMap(){
-    $('#map-canvas').fadeIn(2000);
+    console.log("map initialised");
+    $('#click_me').fadeOut(2000);
+    $('.map_canvas_container').fadeIn(2000);
     initialize();
 };
 
